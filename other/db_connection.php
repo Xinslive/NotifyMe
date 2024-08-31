888db88b03c886a461b725347149bed9ae12e69d
Xin <102579886+Xinslive@users.noreply.github.com>
2024-08-31 13:33:06 +0800

    Update db_connection.php

--- other/db_connection.php
+++ other/db_connection.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "数据库账号", "数据库密码", "数据库名");
+$mysqli = mysqli_connect("localhost", "数据库账号", "数据库密码", "数据库名");
 if ($mysqli->connect_error) {
-    die("连接失败: " . $mysqli->connect_error);
+    die('数据库连接失败 (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
